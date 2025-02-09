dfd0b776294196837bd6e6f0470fa0143c9b2852
Timur1412 <artik201695@gmail.com>
2025-02-09 16:53:54 +0300

    Add files via upload

---
+++ Домашнее задание4.py
@@ -0,0 +1,4 @@
+a, b, =map(float, input().split())
+print(a * b)
+a, b, =map(int, input().split())
+print(a + b)
---
+++ Домашнее задание5.py
@@ -0,0 +1,8 @@
+a = int(input())
+print(a ** 5)
+b = a ** 5 
+print(b * 2)
+c = b * 2
+print(c / 2)
+d = c/(4 - 6)
+print(d)
